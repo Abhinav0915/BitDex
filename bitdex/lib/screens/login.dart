51da0b02a768fb62829fabbc14272b8798d581eb
Abhinav Saxena <abhinav150601@gmail.com>
2023-04-11 17:36:45 +0530

    added shared preferences

--- bitdex/lib/screens/login.dart
+++ bitdex/lib/screens/login.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:bitdex/constants/appcolors.dart';
 import 'package:bitdex/screens/homepage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -6,6 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 bool _obscureText = true;
+bool _isLoggedIn = false;
 
 class login extends StatefulWidget {
   const login({Key? key}) : super(key: key);
@@ -21,11 +24,19 @@ class _loginState extends State<login> {
   final _textController4 = TextEditingController();
   final _textController5 = TextEditingController();
 
+  SharedPreferences? _prefs;
+
   @override
   void initState() {
     super.initState();
     _textController4.addListener(_onTextChanged4);
     _textController5.addListener(_onTextChanged5);
+    SharedPreferences.getInstance().then((prefs) {
+      setState(() {
+        _prefs = prefs;
+        _isLoggedIn = _prefs?.getBool('loggedIn') ?? false;
+      });
+    });
   }
 
   @override
@@ -49,182 +60,193 @@ class _loginState extends State<login> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: AppColors.black,
-        body: SingleChildScrollView(
-            child: Container(
-          margin: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-          width: double.infinity,
-          child: Column(
-            children: [
-              Image.asset(
-                'assets/images/logo.png',
-                height: 200.0,
-              ),
-              const Text(
-                'Sign In',
-                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              Container(
-                child: SizedBox(
+    bool isLoggedIn = _prefs?.getBool('loggedIn') ?? false;
+    if (isLoggedIn) {
+      return homepage();
+    } else {
+      return Scaffold(
+          backgroundColor: AppColors.black,
+          body: SingleChildScrollView(
+              child: Container(
+            margin: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+            width: double.infinity,
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/images/logo.png',
+                  height: 200.0,
+                ),
+                const Text(
+                  'Sign In',
+                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                Container(
+                  child: SizedBox(
+                      width: 300,
+                      child: TextField(
+                        controller: _textController4,
+                        textInputAction: TextInputAction.next,
+                        style: const TextStyle(
+                          color: AppColors.black,
+                        ),
+                        decoration: const InputDecoration(
+                          contentPadding: EdgeInsets.symmetric(
+                              horizontal: 20.0, vertical: 16.0),
+                          isDense: true,
+                          filled: true,
+                          fillColor: AppColors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20.0),
+                            ),
+                          ),
+                          prefixIcon: Icon(Icons.mail),
+                          prefixIconColor: AppColors.black,
+                          prefixStyle: TextStyle(color: AppColors.black),
+                          hintText: 'Email',
+                          hintStyle: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                        maxLines: 1,
+                      )),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                SizedBox(
                     width: 300,
                     child: TextField(
-                      controller: _textController4,
-                      textInputAction: TextInputAction.next,
+                      controller: _textController5,
+                      textInputAction: TextInputAction.done,
+                      obscureText: _obscureText,
                       style: const TextStyle(
                         color: AppColors.black,
                       ),
-                      decoration: const InputDecoration(
-                        contentPadding: EdgeInsets.symmetric(
+                      decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                          icon: Icon(_obscureText
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                          onPressed: () {
+                            setState(() {
+                              _obscureText = !_obscureText;
+                            });
+                          },
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
                             horizontal: 20.0, vertical: 16.0),
                         isDense: true,
                         filled: true,
-                        fillColor: AppColors.white,
-                        border: OutlineInputBorder(
+                        fillColor: Colors.white,
+                        border: const OutlineInputBorder(
                           borderRadius: BorderRadius.all(
                             Radius.circular(20.0),
                           ),
                         ),
-                        prefixIcon: Icon(Icons.mail),
-                        prefixIconColor: AppColors.black,
-                        prefixStyle: TextStyle(color: AppColors.black),
-                        hintText: 'Email',
-                        hintStyle: TextStyle(
-                          color: Colors.black,
+                        prefixIcon: const Icon(Icons.key),
+                        prefixIconColor: Colors.black,
+                        hintText: 'Password',
+                        hintStyle: const TextStyle(
+                          color: AppColors.black,
                         ),
                       ),
                       maxLines: 1,
                     )),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              SizedBox(
-                  width: 300,
-                  child: TextField(
-                    controller: _textController5,
-                    textInputAction: TextInputAction.done,
-                    obscureText: _obscureText,
-                    style: const TextStyle(
-                      color: AppColors.black,
-                    ),
-                    decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                        icon: Icon(_obscureText
-                            ? Icons.visibility
-                            : Icons.visibility_off),
-                        onPressed: () {
-                          setState(() {
-                            _obscureText = !_obscureText;
-                          });
-                        },
-                      ),
-                      contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 20.0, vertical: 16.0),
-                      isDense: true,
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(20.0),
-                        ),
-                      ),
-                      prefixIcon: const Icon(Icons.key),
-                      prefixIconColor: Colors.black,
-                      hintText: 'Password',
-                      hintStyle: const TextStyle(
-                        color: AppColors.black,
+                const SizedBox(
+                  height: 30.0,
+                ),
+                Container(
+                  width: 250.0,
+                  height: 45.0,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      alignment: Alignment.center,
+                      primary: AppColors.purple,
+                      onPrimary: AppColors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
                       ),
                     ),
-                    maxLines: 1,
-                  )),
-              const SizedBox(
-                height: 30.0,
-              ),
-              Container(
-                width: 250.0,
-                height: 45.0,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    alignment: Alignment.center,
-                    primary: AppColors.purple,
-                    onPrimary: AppColors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                  ),
-                  onPressed: _hasInput4 && _hasInput5
-                      ? () {
-                          FirebaseAuth.instance
-                              .signInWithEmailAndPassword(
-                                email: _textController4.text,
-                                password: _textController5.text,
-                              )
-                              .then((value) => Navigator.push(
+                    onPressed: _hasInput4 && _hasInput5
+                        ? () {
+                            FirebaseAuth.instance
+                                .signInWithEmailAndPassword(
+                              email: _textController4.text,
+                              password: _textController5.text,
+                            )
+                                .then((value) {
+                              Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => const homepage())))
-                              .catchError((error) {
-                            showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    backgroundColor: AppColors.red,
-                                    title: Text("Login Failed"),
-                                    content: Text(error.message),
-                                    actions: <Widget>[
-                                      TextButton(
-                                        child: Text("OK"),
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                        },
-                                      ),
-                                    ],
-                                  );
-                                });
-                          });
-                        }
-                      : null,
-                  child: const Text(
-                    'Log In',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: AppColors.white,
+                                      builder: (context) => const homepage()));
+                              _prefs?.setBool('loggedIn',
+                                  true); // save the authentication state
+                              setState(() {
+                                _isLoggedIn = true;
+                              });
+                            }).catchError((error) {
+                              showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      backgroundColor: AppColors.red,
+                                      title: Text("Login Failed"),
+                                      content: Text(error.message),
+                                      actions: <Widget>[
+                                        TextButton(
+                                          child: Text("OK"),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                        ),
+                                      ],
+                                    );
+                                  });
+                            });
+                          }
+                        : null,
+                    child: const Text(
+                      'Log In',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: AppColors.white,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Row(
-                children: [
-                  const SizedBox(
-                    width: 140.0,
-                  ),
-                  const Text(
-                    'New User?',
-                    style: TextStyle(
-                      fontSize: 15.0,
-                      color: Colors.white,
+                Row(
+                  children: [
+                    const SizedBox(
+                      width: 140.0,
                     ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/signup');
-                    },
-                    child: const Text(
-                      'Sign Up',
+                    const Text(
+                      'New User?',
                       style: TextStyle(
                         fontSize: 15.0,
                         color: Colors.white,
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        )));
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/signup');
+                      },
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )));
+    }
   }
 }

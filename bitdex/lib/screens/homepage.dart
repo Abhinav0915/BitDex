88b0a7d5286ba61c1de8e74bd923adf4f9530b32
Abhinav Saxena <abhinav150601@gmail.com>
2023-07-25 16:34:50 +0530

    bug fixed

--- bitdex/lib/screens/homepage.dart
+++ bitdex/lib/screens/homepage.dart
@@ -28,6 +28,7 @@ class _homepageState extends State<homepage> {
       _filterCryptoData(_searchController.text);
     });
     // _filteredCryptoData = List.from(_cryptoData);
+    // _filteredCryptoData = List.from(_cryptoData);
     // _filterCryptoData(_searchController.text);
   }
 
@@ -105,152 +106,152 @@ class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('BitDex'),
-          actions: [
-            IconButton(
-              onPressed: () {
-                setState(() {
-                  _isSearching = !_isSearching;
-                  if (!_isSearching) {
-                    _searchController.clear();
-                    _filteredCryptoData = _cryptoData;
-                  }
-                });
-              },
-              icon: _isSearching
-                  ? const Icon(Icons.clear)
-                  : const Icon(Icons.search),
+      appBar: AppBar(
+        title: const Text('BitDex'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                _isSearching = !_isSearching;
+                if (!_isSearching) {
+                  _searchController.clear();
+                  _filteredCryptoData = _cryptoData;
+                }
+              });
+            },
+            icon: _isSearching
+                ? const Icon(Icons.clear)
+                : const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: _showSortOptions,
+            icon: const Icon(Icons.sort),
+          ),
+        ],
+        backgroundColor: Colors.black,
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.black,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.white54,
+              ),
+              child: Text('Drawer Header'),
             ),
-            IconButton(
-              onPressed: _showSortOptions,
-              icon: const Icon(Icons.sort),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Homepage'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.favorite),
+              title: const Text('Favorites'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FavoriteScreen()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('Log Out'),
+              leading: const Icon(Icons.logout),
+              onTap: () async {
+                // print('Log out tapped');
+                final prefs = await SharedPreferences.getInstance();
+                await prefs.clear();
+                FirebaseAuth.instance.signOut();
+                // ignore: use_build_context_synchronously
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const login()),
+                );
+              },
             ),
           ],
-          backgroundColor: Colors.black,
-          centerTitle: true,
         ),
-        drawer: Drawer(
-          backgroundColor: Colors.black,
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.white54,
-                ),
-                child: Text('Drawer Header'),
-              ),
-              ListTile(
-                leading: const Icon(Icons.home),
-                title: const Text('Homepage'),
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.favorite),
-                title: const Text('Favorites'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) =>  FavoriteScreen()),
-                  );
-                },
-              ),
-              ListTile(
-                title: const Text('Log Out'),
-                leading: const Icon(Icons.logout),
-                onTap: () async {
-                  // print('Log out tapped');
-                  final prefs = await SharedPreferences.getInstance();
-                  await prefs.clear();
-                  FirebaseAuth.instance.signOut();
-                  // ignore: use_build_context_synchronously
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => const login()),
-                  );
-                },
-              ),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.pink,
+              Color.fromARGB(255, 88, 1, 201),
+              Colors.black87,
             ],
           ),
         ),
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Colors.pink,
-                Color.fromARGB(255, 88, 1, 201),
-                Colors.black87,
-              ],
-            ),
-          ),
-          child: Column(
-            children: [
-              AnimatedContainer(
-                duration: const Duration(milliseconds: 550),
-                width: _isSearching
-                    ? MediaQuery.of(context).size.width * 0.6
-                    : 0.0,
-                child: TextField(
-                  controller: _searchController,
-                  onChanged: (value) {
-                    _filterCryptoData(value);
-                  },
-                  decoration: const InputDecoration(
-                    hintText: 'Search Cryptocurrency',
-                    hintStyle:
-                        TextStyle(color: Color.fromARGB(143, 255, 255, 255)),
-                    border: InputBorder.none,
-                  ),
+        child: Column(
+          children: [
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 550),
+              width:
+                  _isSearching ? MediaQuery.of(context).size.width * 0.6 : 0.0,
+              child: TextField(
+                controller: _searchController,
+                onChanged: (value) {
+                  _filterCryptoData(value);
+                },
+                decoration: const InputDecoration(
+                  hintText: 'Search Cryptocurrency',
+                  hintStyle:
+                      TextStyle(color: Color.fromARGB(143, 255, 255, 255)),
+                  border: InputBorder.none,
                 ),
               ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: _filteredCryptoData.length,//_cryptoData.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    final data = _filteredCryptoData[index];//_cryptoData[index];
-                    final price =
-                        double.parse(data['quote']['USD']['price'].toString());
-                    final priceString = price.toStringAsFixed(2);
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _cryptoData.length, //_cryptoData.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final data = _cryptoData[index]; //_cryptoData[index];
+                  final price =
+                      double.parse(data['quote']['USD']['price'].toString());
+                  final priceString = price.toStringAsFixed(2);
 
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.pushNamed(context, '/cryptodetails',
-                            arguments: data);
-                      },
-                      child: Card(
-                        elevation: 0.0,
-                        color: Colors.transparent,
-                        child: ListTile(
-                          leading: Image.network(
-                            'https://s2.coinmarketcap.com/static/img/coins/64x64/${data['id']}.png',
-                            height: 40.0,
-                            width: 40.0,
-                          ),
-                          title: Text(
-                            data['name'],
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          trailing: Text(
-                            '\$$priceString',
-                            style: const TextStyle(
-                              color: Colors.green,
-                              fontSize: 16.0,
-                              fontWeight: FontWeight.bold,
-                            ),
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/cryptodetails',
+                          arguments: data);
+                    },
+                    child: Card(
+                      elevation: 0.0,
+                      color: Colors.transparent,
+                      child: ListTile(
+                        leading: Image.network(
+                          'https://s2.coinmarketcap.com/static/img/coins/64x64/${data['id']}.png',
+                          height: 40.0,
+                          width: 40.0,
+                        ),
+                        title: Text(
+                          data['name'],
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        trailing: Text(
+                          '\$$priceString',
+                          style: const TextStyle(
+                            color: Colors.green,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
-                    );
-                  },
-                ),
+                    ),
+                  );
+                },
               ),
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
